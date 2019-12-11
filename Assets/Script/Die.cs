54fc2d98544a5bd42f3123d4b50d1e4dee8913ff
developerakash <akashhussain606@gmail.com>
2019-12-11 10:44:08 +0600

    Initial commit

---
+++ Assets/Script/Die.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    public GameObject Bird;
+    public GameObject pattern;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "ground")
+        {
+            Destroy();
+            
+        }
+    }
+
+    private void Destroy()
+    {
+        Destroy(Bird);
+        Instantiate(pattern,transform.position, Quaternion.identity);
+    }
+}
